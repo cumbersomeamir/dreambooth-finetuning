c6eecf401f50db22af2eef91704344328aaf4aa3
Amir Kidwai <findamirkidwai@gmail.com>
2023-12-06 18:52:10 +0530

    Create loading-dataset-and-model.py

---
+++ loading-dataset-and-model.py
@@ -0,0 +1,115 @@
+#Loading Base Model and VAE Decoder
+from diffusers import DiffusionPipeline, AutoencoderKL
+import torch
+
+vae = AutoencoderKL.from_pretrained(
+    "madebyollin/sdxl-vae-fp16-fix", 
+    torch_dtype=torch.float16
+)
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    vae=vae,
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True,
+)
+pipe.to("cuda");
+
+print("Model Loading completed")
+
+#Running the image generation pipeline
+prompt = "A man in a spacesuit is running a marathon in the jungle."
+image = pipe(prompt=prompt, num_inference_steps=25, num_images_per_prompt = 4)
+
+#Display images in a grid
+from PIL import Image
+
+def image_grid(imgs, rows, cols, resize=256):
+    assert len(imgs) == rows * cols
+
+    if resize is not None:
+        imgs = [img.resize((resize, resize)) for img in imgs]
+
+    w, h = imgs[0].size
+    grid_w, grid_h = cols * w, rows * h
+    grid = Image.new("RGB", size=(grid_w, grid_h))
+
+    for i, img in enumerate(imgs):
+        x = i % cols * w
+        y = i // cols * h
+        grid.paste(img, box=(x, y))
+
+    return grid
+
+#Calling the image_grid function  
+image_grid(image.images, 2, 2)
+
+print("Image generation complete")
+
+#o further improve the image quality and model accuracy, we will use Refiner.
+refiner = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    vae=vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+refiner.to("cuda")
+
+n_steps = 40
+high_noise_frac = 0.7
+
+image = pipe(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_end=high_noise_frac,
+    output_type="latent",
+).images
+
+image = refiner(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_start=high_noise_frac,
+    image=image,
+).images[0]
+
+image
+
+
+#
+PROJECT_NAME = "Dreambooth_SDXL"
+MODEL_NAME = "stabilityai/stable-diffusion-xl-base-1.0"
+DATA_DIR = "/kaggle/input/abids-photos"
+REPO_ID = "kingabzpro/sdxl-lora-abid" #Huggingface repo id
+
+
+from kaggle_secrets import UserSecretsClient
+user_secrets = UserSecretsClient()
+HF_TOKEN = user_secrets.get_secret("HUGGINGFACE_TOKEN")
+
+
+#Display uploaded images
+from PIL import Image
+
+def image_grid(imgs, rows, cols, resize=256):
+    assert len(imgs) == rows * cols
+
+    if resize is not None:
+        imgs = [img.resize((resize, resize)) for img in imgs]
+
+    w, h = imgs[0].size
+    grid_w, grid_h = cols * w, rows * h
+    grid = Image.new("RGB", size=(grid_w, grid_h))
+
+    for i, img in enumerate(imgs):
+        x = i % cols * w
+        y = i // cols * h
+        grid.paste(img, box=(x, y))
+
+    return grid
+
+import glob
+
+print("The following are the images for finetune")
+imgs = [Image.open(path) for path in glob.glob("/kaggle/input/abids-photos/*.jpg")]
+image_grid(imgs, 1, 5)
